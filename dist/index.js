b2a1ab762e66cdf233b56688f76d336fb78cd733
tieguanyin <tieguanyin@wacai.com>
2018-01-19 12:09:48 +0800

    Add build files to git

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+var rp = require('request-promise');
+
+var _require = require('js-base64'),
+    Base64 = _require.Base64;
+
+var _require2 = require('./constants'),
+    SERVICE_URL = _require2.SERVICE_URL,
+    OUTPUT_TEMPLATE = _require2.OUTPUT_TEMPLATE;
+
+function getContentPath(kwargs) {
+  var rel = kwargs.relativeTo;
+  var basePath = rel ? _.isString(rel.path) ? path.dirname(rel.path) : rel : '.';
+  var absPath = path.resolve(basePath, kwargs.src);
+
+  return path.relative(this.resolve('.'), absPath);
+}
+
+function getConfig(context, property, defaultValue) {
+  var config = _.get(context, 'config') ? _.get(context, 'config') : _.get(context, 'book.config');
+  return config ? config.get(property, defaultValue) : defaultValue;
+}
+
+function loadUmlContent() {
+  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+      _ref$kwargs = _ref.kwargs,
+      kwargs = _ref$kwargs === undefined ? {} : _ref$kwargs,
+      body = _ref.body;
+
+  if (kwargs.src) {
+    return this.readFileAsString(getContentPath.call(this, kwargs));
+  }
+  return Promise.resolve(body);
+}
+
+function trimContent(content) {
+  var indexFrom = content.indexOf('@startuml');
+  var indexTo = content.lastIndexOf('@enduml') + 7;
+
+  if (indexFrom < 0 || indexTo < 0) {
+    throw new Error('Invalid PlantUML content');
+  }
+  return content.slice(indexFrom, indexTo);
+}
+
+function renderUml(content) {
+  var params = {
+    url: getConfig(this, 'pluginsConfigPlantumlSvgServiceUrl', SERVICE_URL),
+    body: content
+  };
+
+  var config = getConfig(this, 'pluginsConfig.plantuml-svg.config');
+  if (config) {
+    params.qs = { config: config };
+    params.useQuerystring = true;
+  }
+
+  return (this.rpMock || rp).post(params).then(Base64.encode).then(OUTPUT_TEMPLATE);
+}
+
+module.exports = {
+  blocks: {
+    uml: {
+      process: function process(block) {
+        return loadUmlContent.call(this, block).then(trimContent).then(renderUml.bind(this));
+      }
+    }
+  }
+};
